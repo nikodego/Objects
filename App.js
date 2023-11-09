e57447b39b813b1e44271e6945426370e4a2646d
nikodego <nikkobryllealturas716@gmail.com>
2023-11-09 15:15:00 +1100

    1st commit

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList, ScrollView } from 'react-native';
+import { Icon } from '@rneui/themed';
+import Entypo from 'react-native-vector-icons/Entypo';
+import { datas } from './data';
 
 export default function App() {
+
+  const fullName = (users) =>{
+    alert(users)
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+      <Entypo/>
+      <FlatList
+        data={datas}
+        keyExtractor={(index) => index.toString()}
+        renderItem={({ item }) => (
+        <View style={styles.user}>
+          <Image
+            style={styles.image}
+            resizeMode="cover"
+            source={{ uri: item.avatar }}
+          />
+        <View>
+          <Text style={styles.name}>{item.name}</Text>
+          <Text style={styles.address}>{item.address}</Text>
+        </View>
+        <View style={{flex:1}}>
+        <TouchableOpacity onPress={()=>fullName(`${item.name}${'\n'}${item.alias}${'\n'}${item.position}${'\n'}${item.address}${'\n'}${item.bounty}`)}>
+          <Icon
+            name="dots-three-horizontal"
+            type="entypo"
+            size={20}
+            style={styles.opt}
+        />
+        </TouchableOpacity>
+        </View> 
+    </View>
+  )}
+    />
+    </ScrollView>
     </View>
   );
 }
@@ -16,5 +52,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20,
+    padding: 20,
+  },
+  user: {
+    flexDirection: 'row',
+    marginBottom: 6,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 20,
+    borderRadius:400/2,
+  },
+  name: {
+    fontSize: 16,
+    marginTop: 5,
+    fontWeight:'bold',
+  },
+  address:{
+    fontSize: 12,
+    marginBottom:20
+  },
+  opt:{
+    flexDirection:'row-reverse',
+    padding:12,
+    marginLeft:15
   },
 });
---
+++ data.js
@@ -0,0 +1,92 @@
+export const datas = [
+    {
+    name: 'Monkey D. Luffy',
+    alias: 'Straw Hat',
+    position:'Captain',
+    address:'East Blue',
+    bounty:'3,000,000,000',
+    avatar:
+        'https://i.pinimg.com/originals/f0/af/cb/f0afcbce7ed4a7df7b822964501bf995.jpg',
+    },
+    {
+    name: 'Roronoa Zoro',
+    alias: 'Pirate Hunter',
+    position:'Swordsman',
+    address:'East Blue',
+    bounty:'1,111,000,000',
+    avatar:
+        'https://images4.alphacoders.com/122/thumb-1920-1228259.png',
+    },
+    {
+    name: 'Nami',
+    alias: 'Cat Burglar',
+    position:'Navigator',
+    address:'East Blue',
+    bounty:'366,000,000',
+    avatar:
+        'https://staticg.sportskeeda.com/editor/2023/03/474e0-16794903651247-1920.jpg?w=840',
+    },
+    {
+    name: 'Ussop',
+    alias: 'God Ussop',
+    position:'Sniper',
+    address:'East Blue',
+    bounty:'500,000,000',
+    avatar:
+        'https://sportshub.cbsistatic.com/i/2021/03/18/08081697-cdac-4e36-91b5-10df6a5d9015/one-piece-usopp-wano-opening-23-1231730.jpg',
+    },
+    {
+    name: 'Sanji',
+    alias: 'Black Leg',
+    position:'Cook',
+    address:'East Blue',
+    bounty:'1,032,000,000',
+    avatar:
+        'https://staticg.sportskeeda.com/editor/2023/05/425f5-16829548801476-1920.jpg?w=840',
+    },
+    {
+    name: 'Tony Tony Chopper',
+    alias: 'Cotton Candy Lover',
+    position:'Doctor',
+    address:'Grand Line',
+    bounty:'1,000',
+    avatar:
+        'https://sportshub.cbsistatic.com/i/2021/03/18/f81c34c4-97f9-4523-b68e-74765a6d209a/one-piece-1237049.jpg',
+    },
+    {
+    name: 'Nico Robin',
+    alias: 'Demon Child',
+    position:'Archeologist',
+    address:'West Blue',
+    bounty:'930,000,000',
+    avatar:
+        'https://storage.googleapis.com/cdn.vcgamers.com/news/wp-content/uploads/2023/10/nico-robin.jpg',
+    },
+    {
+    name: 'Franky',
+    alias: 'Cyborg',
+    position:'Shipwright',
+    address:'Grand Line',
+    bounty:'394,000,000',
+    avatar:
+        'https://staticg.sportskeeda.com/editor/2023/05/50b87-16836967920057-1920.jpg?w=840',
+    },
+    {
+    name: 'Brook',
+    alias: 'Soul King',
+    position:'Musician',
+    address:'West Blue',
+    bounty:'383,000,000',
+    avatar:
+        'https://lh4.googleusercontent.com/nL6FEg_LjLIp7uLl-aeN26JOsMkPCL9NiggwQRpBprGV-I5Uu6otVFLZNf45GOX7lUdP2I-bQJvZL8tJ6mu3kHbaIV-ucscUiEUo_Lir22Jwzf-uzIasdfQjWVmlYVkU2C7pKtosNA-v2oQwqGjl0co',
+    },
+    {
+    name: 'Jimbei',
+    alias: 'Knight of the Sea',
+    position:'Helmsman',
+    address:'New World',
+    bounty:'1,100,000,000',
+    avatar:
+        'https://i.pinimg.com/736x/23/96/43/239643b6fcd730c95a5019fdc642b5c6.jpg',
+    },
+];
